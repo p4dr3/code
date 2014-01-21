c7b47b4f0eed9d098cab939034be40a7fc130032
unknown <dusautax@MW7252.InterDigital.com>
2014-01-21 17:35:59 -0500

    added unicode support added html parsing tested with other rss sources

--- reveil.py
+++ reveil.py
@@ -2,7 +2,14 @@
 from urllib import urlopen
 from bs4 import BeautifulSoup
 
-rss_link = "http://rss.lemonde.fr/c/205/f/3050/index.rss"
+# works
+#rss_link = "http://rss.lemonde.fr/c/205/f/3050/index.rss"
+rss_link = "http://rss.lapresse.ca/225.xml"
+
+#doesnt work
+#rss_link = "http://feeds.gawker.com/lifehacker/full"
+
+
 
 #read webpage and stores it
 webpage = urlopen(rss_link).read()
@@ -12,17 +19,32 @@ mysoup = BeautifulSoup(webpage)
 
 #parsing
 titlesoup = mysoup.findAll('title')
+descrsoup = mysoup.findAll('description')
 
 #debug
 #print titlesoup
 
 list=[]
-list[:]=range(2,10)
+list[:]=range(2,10) #27)
 
 for i in list:
-    toto = str(titlesoup[i]) #force to string
+    #force to unicode string ( instead of str() )
+    toto = unicode(titlesoup[i]) 
+    tata = unicode(descrsoup[i-1])
+    
+    
     #remove html tags
     toto = toto.replace("<title>", "")
     toto = toto.replace("</title>", "")
+    
+    tata = tata.replace("<description>", "")
+    #get rid of the html after the description
+    tata = tata.split ('&lt')[0]
+    
     print toto
+    print tata
+    print ""
+    
+   
+    
     
