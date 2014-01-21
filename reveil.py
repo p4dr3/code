d0066e02cee5289b995a648dad429f413e6d91e5
unknown <dusautax@MW7252.InterDigital.com>
2014-01-21 14:48:09 -0500

    proof of concept

---
+++ reveil.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+
+from urllib import urlopen
+from bs4 import BeautifulSoup
+
+rss_link = "http://rss.lemonde.fr/c/205/f/3050/index.rss"
+
+
+webpage = urlopen(rss_link).read()
+mysoup = BeautifulSoup(webpage)
+
+titlesoup = mysoup.findAll('title')
+
+bla = "<title> 1 <title> 23 <title>"
+print bla.replace("<title>","")
+print bla
+
+for i in range(2,10):
+    bla2 = string(titlesoup[i])
+    print bla2.replace("<title>","")
+
+
+def mafonction(a,b):
+    return a+b
+
+
+print mafonction(10,20)
