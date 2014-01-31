c40782bf2f45cec2e424417098f646de9739eaee
unknown <dusautax@MW7252.InterDigital.com>
2014-01-31 17:19:30 -0500

    Updated Salli support, cookies and redirect support, binary retrieved

--- salli.py
+++ salli.py
@@ -1,9 +1,7 @@
 #! /usr/bin/python
-from urllib import urlopen
-import urllib2
-import re
 import base64
-
+import requests
+import StringIO
 #http://base64encode.net/ and replace = by .
 
 def encode_it(data):
@@ -12,13 +10,13 @@ def encode_it(data):
     return data
 
 def get_session_id(website):
-    webpage = urlopen(website).readlines()
-    for line in webpage:
+    buf = StringIO.StringIO(website)
+    for line in buf:
         if line.find("voicetest.php?rtr")!= -1: 
             line = line.split("&",10)
             line = line[4].split("\"",2)
             session_id = line[0]
-            print "Session _id: "+session_id
+            #print "Session _id: "+session_id
             return session_id
     
 
@@ -32,33 +30,50 @@ voice_id = "en_us_salli" #Salli
 voice_id = encode_it(voice_id)
 
 
-session_id = get_session_id(website)
-
-
-my_url = website+script+"?rtr=1&t2r="+text+"&v2r="+voice_id+"&lang=en&"+session_id
-print my_url
-
-#!- validated
-
-#experimental
+#header mimicing firfox
 my_header = {"User-Agent":
-       "Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7"}
+       "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0"}
+
+#REQUESTS 
+
+#setup a session to keep cookies
+session = requests.Session()
+print "SESSION"
+print session.cookies
+
+#grabbing session id and cookie from 1st connection
+response1 = session.request('GET', website)
+#print response1
+print "REQ1 - COOKIES"
+print response1.cookies
+
+session_id = get_session_id(response1.content) #webpage
+
+#formating and sending http GET request
+my_url = website+script
+parameters = "rtr=1&t2r="+text+"&v2r="+voice_id+"&lang=en&"+session_id
+
+myreq = requests.Request(method='GET', url = my_url, params = parameters , cookies = session.cookies )#, stream=True) #, allow_redirect =True
+req = myreq.prepare()
+response2 = session.send(req)
+
+#Making sure we got what we wanted
+
+print "REQ2 - URL"
+print response2.url
+print "REQ2 - HEADERS"
+print response2.headers
+my_mp3 = response2.content #that's my MP3
+print "REQ2 - HISTORY"
+print response2.history
+
+#print my_mp3
+
+#f = open('my_mp3.wav','w')
+#f.write(response2.content) # python will convert \n to os.linesep
+#f.close()
 
 
-my_req = urllib2.Request(my_url, "", my_header)
-response = urllib2.urlopen(my_url)    
-page = response.read()
-print "toto"
-print page
-#s = Sound() 
-##s.read('sound.wav') 
-#s.play()
-          
-          
-         
-#print webpage.split()
 
 
-#print webpage(44060,40)
-#for line in webpage.index:
- #   print line
+
